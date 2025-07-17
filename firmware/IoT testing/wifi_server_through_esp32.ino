f10ca9a1a4d74de94054eb968bf4cdd98ef5462d
Oshadha_Sam <samarakoonf@gmail.com>
2025-07-17 14:02:46 +0530

    Update wifi_server_through_esp32.ino

---
+++ firmware/IoT testing/wifi_server_through_esp32.ino
@@ -0,0 +1,30 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "ESP32_Audio";
+const char* password = "12345678";  // Must be at least 8 chars
+
+WebServer server(80);
+
+const int micPin = 34;
+
+void handleData() {
+  int soundVal = analogRead(micPin);
+  server.send(200, "text/plain", String(soundVal));
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  Serial.println("Access Point Started");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());  // usually 192.168.4.1
+
+  server.on("/get-sound", handleData);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
